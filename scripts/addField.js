a12c7606c33d71279ad02539aa126da4200711bb
remilsonpassos <remilsonpassos@gmail.com>
2020-08-08 12:17:31 -0300

    Proffy

---
+++ scripts/addField.js
@@ -0,0 +1,18 @@
+
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+
+
+function cloneField(){
+
+   const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    const fields = newFieldContainer.querySelectorAll('input')
+    
+    fields.forEach(function(field){
+        fields.value = ""
+    })
+
+
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
